7c1e7e6fe805141feb7664f396d565a888f833ce
Max <kamenevms96@gmail.com>
2018-07-16 14:31:21 +0300

    Develop (#1)
    * Initial Google Oauth2
    
    * [maven-release-plugin] prepare release v0.0.1

--- src/main/java/com/tdk/edu/chatterino/ChatterinoApplication.java
+++ src/main/java/com/tdk/edu/chatterino/ChatterinoApplication.java
@@ -2,8 +2,10 @@ package com.tdk.edu.chatterino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class ChatterinoApplication {
 
 	public static void main(String[] args) {
